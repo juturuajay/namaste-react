fd55e9bc953f6b17fac0214f2a5b618683ab767b
juturuajay <86615742+juturuajay@users.noreply.github.com>
2023-04-19 21:24:19 +0530

    episode-02-03

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const HeadingComponent = () => (
+       <div id="container">
+            <h6>{Title()} </h6> 
+            {Title()}          
+            <h1>Namaste React using Functional Component</h1>
+            <HeadingComponent2 />
+        </div>
+);
+
+const HeadingComponent2 = () => (
+    <h1>Namaste React using F2</h1>
+)
+
+const Title = () => (
+    <>
+     <h1 className="heading">Namaste React</h1>
+     <HeadingComponent2 />
+     <HeadingComponent2></HeadingComponent2>
+     </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
---
+++ README.md
@@ -0,0 +1,17 @@
+#namaste react
+
+
+#Parcel
+- Dev build
+- Local server
+- HMR- Hot module replacement
+- File Watching Algorithm - written in C++
+- Caching - Fatser builds
+- Image optimization
+- Minification
+- Compressing
+- Diagnostics
+- Https
+- Tree shaking- remove unused code
+
+
