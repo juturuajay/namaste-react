d46e6b91d0ea1045274fa37ed3b1f9c4b21fa627
juturuajay <86615742+juturuajay@users.noreply.github.com>
2023-08-29 21:19:18 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,16 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "i'm from H1 main tag"),
-    React.createElement("h2", {}, "i'm from H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "i'm from H1 tag"),
-    React.createElement("h2", {}, "i'm from H2 tag"),
-  ]),
-]);
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
